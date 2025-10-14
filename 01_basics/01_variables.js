dcddd15cb3ba1baf6d0ee4c6937ad65979730341
Zaid123-hub <zaidsayyed09876@gmail.com>
2025-10-14 18:43:52 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,14 +1,20 @@
 const accountId = 144533
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
-accountCity = "jabalpur"
-let accountState;
+let accountCity = "jabalpur"
+// let accountState;
 
 // accountId = 2 //not allowed because it is a const and cannot be changed
+
+// prefer not to use var 
+// beacuse of issue in block scope and functional scope
+
 
 accountEmail = "hc@hc.com"
 accountPassword = "21212121"
 accountCity = "Bengaluru"
 
+
 console.log(accountId)
 
+console.table([accountId,accountEmail,accountPassword,accountCity])
