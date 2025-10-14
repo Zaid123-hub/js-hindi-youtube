d0045e99c357e6d954754c168f5566cb20f32602
Zaid123-hub <zaidsayyed09876@gmail.com>
2025-10-14 18:48:19 +0000

    variablesand datatypes changes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,20 +1,22 @@
-const accountId = 144533
+const accountId = 144553
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
-let accountCity = "jabalpur"
-// let accountState;
+accountCity = "Jaipur"
+let accountState;
 
-// accountId = 2 //not allowed because it is a const and cannot be changed
-
-// prefer not to use var 
-// beacuse of issue in block scope and functional scope
+// accountId = 2 // not allowed
 
 
 accountEmail = "hc@hc.com"
 accountPassword = "21212121"
 accountCity = "Bengaluru"
 
+console.log(accountId);
 
-console.log(accountId)
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
 
-console.table([accountId,accountEmail,accountPassword,accountCity])
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
