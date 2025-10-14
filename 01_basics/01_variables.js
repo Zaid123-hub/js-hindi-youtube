d3bf60db6557613ecd7d119376435545d0b93c0e
Zaid123-hub <zaidsayyed09876@gmail.com>
2025-10-14 18:18:08 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144533
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "jabalpur"
+let accountState;
+
+// accountId = 2 //not allowed because it is a const and cannot be changed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId)
+
