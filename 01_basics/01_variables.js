34e2c20885e3e6fa55c8935ed9899ce7195e7591
Zaid123-hub <zaidsayyed09876@gmail.com>
2025-10-20 14:32:03 +0530

    comparison and cnoversion changes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,19 @@
-const accountId = 144553
+const accountId = 144533
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "jabalpur"
 let accountState;
+accountCity = "Jaipur"
+
+
+
+// accountId = 2 //not allowed because it is a const and cannot be changed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId)
 
 // accountId = 2 // not allowed
 
