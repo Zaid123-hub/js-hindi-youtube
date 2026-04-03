c7d3f83d5b4a99cf6cddc4a438dbf88c0361b38e
Zaid123-hub <zaidsayyed09876@gmail.com>
2026-04-03 22:28:20 +0530

    change

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -46,7 +46,8 @@ let str2 = " hitesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
+// console.log("1" + 2); agr string 1st h to sb string me convert ho jayega
+//otherwise agr string baad me h and operation ho rha h kuch pehle to pehle operation hoga
 // console.log(1 + "2");
 // console.log("1" + 2 + 2);
 // console.log(1 + 2 + "2");
